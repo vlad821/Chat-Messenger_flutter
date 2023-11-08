b4b268ba70170696060495027e2b62711d41b107
vlad821 <115928876+vlad821@users.noreply.github.com>
2023-11-08 21:59:34 +0200

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demoo',
       theme: ThemeData(),
       home: AuthGate(),
     );
